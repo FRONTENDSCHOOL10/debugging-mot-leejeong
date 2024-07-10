e1d989ae1387e5e74eb2eee6c5e24b2bb23ec2fb
hyesu-jeong <hyeppy739@gmail.com>
2024-07-10 18:12:00 +0900

    docs: config input 수정

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        login: resolve(__dirname, 'src/pages/landing/index.html'),
+        landing: resolve(__dirname, 'src/pages/landing/index.html'),
       },
     },
   },
