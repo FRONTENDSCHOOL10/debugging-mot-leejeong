f876254cde3da677df5e51bad18e5ac88507f91e
clouood <clouood111m@gmail.com>
2024-07-10 17:31:43 +0900

    vite.config.js 파일 헤더 경로 수정

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        header: resolve(__dirname, 'src/pages/header/index.html'),
+        header: resolve(__dirname, 'src/layouts/index.html'),
       },
       },
     },
