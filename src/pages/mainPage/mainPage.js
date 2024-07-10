4a30c5c4bbfa4352871bab54d70bf25790789d0d
이영빈 <gmlyb@naver.com>
2024-07-10 15:10:22 +0900

    메인페이지 디자인 일부 수정 , 스와이퍼 처리 테스트 디자인 수정되어야함

---
+++ src/pages/mainPage/mainPage.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+  });
+
+  // 클릭 이벤트
+  document
+    .querySelector('.swiper-button-prev')
+    .addEventListener('click', function () {
+      swiper.slidePrev();
+    });
+  document
+    .querySelector('.swiper-button-next')
+    .addEventListener('click', function () {
+      swiper.slideNext();
+    });
+});
