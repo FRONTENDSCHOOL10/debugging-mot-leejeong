585c0ece6e4f2c34f4a17bd5b9b2988d2e992daa
JubiJung <devvjubi@gmail.com>
2024-07-10 20:26:41 +0900

    style:로그인 페이지 및 아이디 찾기 페이지 스타일링/html 조금 수정

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,15 @@
-import { resolve } from 'node:path'
-import { defineConfig } from 'vite'
-
-
-
-
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
-    outDir:'docs',
+    outDir: 'docs',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        
+        login: resolve(__dirname, 'src/pages/login/index.html'),
+        findid: resolve(__dirname, 'src/pages/findId/index.html'),
       },
     },
-    
   },
-})
+});
