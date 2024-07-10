9c31ef57cb5c1e108f6a21f0268c5938443045e2
clouood <clouood111m@gmail.com>
2024-07-10 20:24:31 +0900

    refactor: main.js, vite.config.js 코드 수정

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,14 @@
-import { resolve } from 'path';
+import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
-const root = resolve(__dirname, 'src');
-const outDir = resolve(__dirname, 'docs');
-const publicDir = resolve(__dirname, 'public');
-
 export default defineConfig({
-  root,
-  publicDir,
   build: {
-    outDir,
-    emptyOutDir: true,
+    outDir: 'docs',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
         header: resolve(__dirname, 'src/layouts/header/index.html'),
       },
-      },
     },
   },
-)
+});
