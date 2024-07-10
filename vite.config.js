84b814228e987514732ba59d620d6e260dc1ccbe
clouood <clouood111m@gmail.com>
2024-07-10 16:18:24 +0900

    vite.config.js 파일 푸터 경로 수정

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
       input: {
         main: resolve(root, 'index.html'),
         landing: resolve(root, 'pages/landing', 'index.html'),
-        footer: resolve(__dirname, 'src/pages/findId/index.html'),
+        footer: resolve(__dirname, 'src/pages/footer/index.html'),
       },
     },
   },
