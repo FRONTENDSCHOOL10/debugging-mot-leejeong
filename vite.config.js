282fbd6087c679ef59a502bbd5d459afe0b82e83
hyesu-jeong <hyeppy739@gmail.com>
2024-07-10 16:31:09 +0900

    fix: root 위치 변경 및 vite.config.js 경로 수정

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,13 @@
-import { resolve } from 'path';
+import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
-const root = resolve(__dirname, 'src');
-const outDir = resolve(__dirname, 'docs');
-const publicDir = resolve(__dirname, 'public');
-
 export default defineConfig({
-  root,
-  publicDir,
   build: {
-    outDir,
-    emptyOutDir: true,
+    outDir: 'docs',
     rollupOptions: {
       input: {
-        main: resolve(root, 'index.html'),
-        landing: resolve(root, 'pages/landing', 'index.html'),
+        main: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'src/pages/landing/index.html'),
       },
     },
   },
