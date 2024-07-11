b6e8a4ac9c6674ce44de942ac4cf60e2cddc07f9
이영빈 <gmlyb@naver.com>
2024-07-11 15:06:23 +0900

    충돌 해결

--- vite.config.js
+++ vite.config.js
@@ -11,11 +11,8 @@ export default defineConfig({
         findid: resolve(__dirname, 'src/pages/findId/index.html'),
         landing: resolve(__dirname, 'src/pages/landing/index.html'),
         footer: resolve(__dirname, 'src/layouts/footer/index.html'),
-<<<<<<< HEAD
         header: resolve(__dirname, 'src/layouts/header/index.html'),
-=======
         modal: resolve(__dirname, 'src/layouts/modal/index.html'),
->>>>>>> origin
         main: resolve(__dirname, 'src/pages/main/index.html'),
 
       },
