2c264477135d1aaab3fc297caf91baf6028a4195
hyesu-jeong <hyeppy739@gmail.com>
2024-07-11 22:35:58 +0900

    feat: 메인 스타일링
    - 상단 배너 영역 스타일 수정
    - 배너 이미지 webp로 변경 및 srcset 셋팅
    - 공통 section클래스 추가 및 스타일링

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const swiper = new Swiper('.swiper-container', {
+  const swiper = new Swiper('.swiper__container', {
     slidesPerView: 1,
     spaceBetween: 30,
     centeredSlides: true,
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: '.swiper__button-next',
+      prevEl: '.swiper__button-prev',
     },
     pagination: {
-      el: '.swiper-pagination',
+      el: '.swiper__pagination',
       clickable: true,
     },
     /* 자동 플레이 설정 */
@@ -19,10 +19,14 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
   // 네비게이션 버튼 클릭 이벤트
-  document.querySelector('.swiper-button-prev').addEventListener('click', function () {
-    swiper.slidePrev();
-  });
-  document.querySelector('.swiper-button-next').addEventListener('click', function () {
-    swiper.slideNext();
-  });
+  document
+    .querySelector('.swiper__button-prev')
+    .addEventListener('click', function () {
+      swiper.slidePrev();
+    });
+  document
+    .querySelector('.swiper__button-next')
+    .addEventListener('click', function () {
+      swiper.slideNext();
+    });
 });
