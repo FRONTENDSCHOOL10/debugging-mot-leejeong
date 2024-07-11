433c3478d91f5d9fa27e307f8c76f1966988e3f3
이영빈 <gmlyb@naver.com>
2024-07-11 15:20:41 +0900

    충돌해결

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    outDir: 'docs',
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'src/pages/login/index.html'),
+        findid: resolve(__dirname, 'src/pages/findId/index.html'),
+        findpwd: resolve(__dirname, 'src/pages/findPwd/index.html'),
+        landing: resolve(__dirname, 'src/pages/landing/index.html'),
+        footer: resolve(__dirname, 'src/layouts/footer/index.html'),
+        modal: resolve(__dirname, 'src/layouts/modal/index.html'),
+        main: resolve(__dirname, 'src/pages/main/index.html'),
+      },
+    },
+  },
+});
