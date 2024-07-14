5971cff5b3681df933eb832a2757c28955eb74e6
clouood <clouood111m@gmail.com>
2024-07-14 14:14:13 +0900

    refactor: header html, scss, js 코드 중간 수정

--- src/layouts/header/header.js
+++ src/layouts/header/header.js
@@ -1,1 +1,20 @@
-import '/src/layouts/header/header.scss';
+import './header.scss';
+
+// header.js
+document.addEventListener('DOMContentLoaded', () => {
+    const profileBtn = document.querySelector('.profile-btn');
+    const profileDropdown = document.querySelector('.header__profile-dropdown');
+
+    profileBtn.addEventListener('click', () => {
+        const isExpanded = profileBtn.getAttribute('aria-expanded') === 'true';
+        profileBtn.setAttribute('aria-expanded', !isExpanded);
+        profileDropdown.setAttribute('aria-hidden', isExpanded);
+    });
+
+    document.addEventListener('click', (event) => {
+        if (!profileBtn.contains(event.target) && !profileDropdown.contains(event.target)) {
+            profileBtn.setAttribute('aria-expanded', false);
+            profileDropdown.setAttribute('aria-hidden', true);
+        }
+    });
+});
