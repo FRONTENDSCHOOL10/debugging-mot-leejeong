d03ca2ceef2f3bf2e4f825462508741e94a36528
hyesu-jeong <hyeppy739@gmail.com>
2024-07-15 11:12:02 +0900

    feat: 랜딩 페이지 스타일 수정 및 메인 페이지 스와이퍼 진행중

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var swiper = new Swiper('.mySwiper', {
+  var swiper = new Swiper('.mainSwiper', {
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
@@ -7,6 +7,18 @@ document.addEventListener('DOMContentLoaded', function () {
     loop: true,
     pagination: {
       el: '.swiper-pagination',
+    },
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  var swiper1 = new Swiper('.contentsSwiper', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
     },
   });
 });
