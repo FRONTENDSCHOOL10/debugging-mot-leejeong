698513fff2a6a3ccf406f3fb9f54bd802083f1fb
이영빈 <gmlyb@naver.com>
2024-07-15 12:18:24 +0900

    환경

---
+++ src/utils/RandomString.js
@@ -0,0 +1,10 @@
+export const RansomString = (num) => {
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()-_+=";
+  let result = "";
+  const charactersLength = characters.length;
+  for(let i=0; i< num; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+};
