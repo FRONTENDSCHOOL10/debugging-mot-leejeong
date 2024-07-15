fca7df9e4f306be7295b1d50edb79124e513b496
이영빈 <gmlyb@naver.com>
2024-07-16 00:15:35 +0900

    이미지 데이터 받아오기 작업중1

--- src/pages/main/feature.js
+++ src/pages/main/feature.js
@@ -17,20 +17,28 @@ async function renderVisualList() {
    visualData.forEach((item) => {
       console.log("item:",item);
       const template = 
-      `
-        <div class="swiper contentsSwiper">
-          <div class="swiper-wrapper">
-            <div class="swiper-slide"> 
-              <div class="thumbnail-wrapper">
+    //   `
+    //     <div class="swiper contentsSwiper">
+    //       <div class="swiper-wrapper">
+    //         <div class="swiper-slide"> 
+    //           <div class="thumbnail-wrapper">
+    //             <img src="${getPbImageURL(item)}" alt="${item.alt}" />
+    //           </div>
+    //           <div class="main-title paragraph-medium">${item.name}</div>
+    //         </div>
+    //       </div>
+    //       <div class="swiper-button-next"></div>
+    //       <div class="swiper-button-prev"></div>
+    //   </div>
+    // `      
+        `
+          <div class="swiper-slide">
+            <div class="thumbnail-wrapper">
                 <img src="${getPbImageURL(item)}" alt="${item.alt}" />
-              </div>
-              <div class="main-title paragraph-medium">${item.name}</div>
             </div>
+            <div class="main-title paragraph-medium">${item.name}</div>
           </div>
-          <div class="swiper-button-next"></div>
-          <div class="swiper-button-prev"></div>
-      </div>
-    `      
+        `;
         console.log(template);
         insertLast('.swiper-wrapper',template);
     });
