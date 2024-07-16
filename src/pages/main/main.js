45142c472fbb500f589b3c676e3a0d26ddb542ce
이영빈 <gmlyb@naver.com>
2024-07-16 09:46:16 +0900

    main.js, feature.js 합침

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,4 +1,8 @@
 import './main.scss';
+import getPbImageURL from '/src/api/getPbImageURL';
+import {comma, getStorage, insertLast ,setDocumentTitle, setStorage,tiger} from 'kind-tiger';
+import PocketBase from 'pocketbase';
+
 
 document.addEventListener('DOMContentLoaded', function () {
   var swiper = new Swiper('.mainSwiper', {
@@ -29,3 +33,51 @@ document.addEventListener('DOMContentLoaded', function () {
     },
   });
 });
+
+
+// 이미지 렌더링 작업
+const pb = new PocketBase('http://127.0.0.1:8090/' ) ;
+
+async function renderVisualList() {
+ 
+    const visualData = await pb.collection('contents').getFullList
+    ({
+      sort: '-created'
+    });
+    
+   console.log(visualData);
+
+   visualData.forEach((item) => {
+      console.log("item:",item);
+      const template = 
+    //   `
+    //     <div class="swiper contentsSwiper">
+    //       <div class="swiper-wrapper">
+    //         <div class="swiper-slide"> 
+    //           <div class="thumbnail-wrapper">
+    //             <img src="${getPbImageURL(item)}" alt="${item.alt}" />
+    //           </div>
+    //           <div class="main-title paragraph-medium">${item.name}</div>
+    //         </div>
+    //       </div>
+    //       <div class="swiper-button-next"></div>
+    //       <div class="swiper-button-prev"></div>
+    //   </div>
+    // `      
+        `
+          <div class="swiper-slide">
+            <div class="thumbnail-wrapper">
+                <img src="${getPbImageURL(item)}" alt="${item.alt}" />
+            </div>
+            <div class="main-title paragraph-medium">${item.name}</div>
+          </div>
+        `;
+        console.log(template);
+        insertLast('.swiper-wrapper',template);
+    });
+
+}
+
+renderVisualList(); 
+
+
