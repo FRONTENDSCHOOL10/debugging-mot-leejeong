41e981c06dbdbd8099950939b5726a89d57fdc3d
hyesu-jeong <hyeppy739@gmail.com>
2024-07-17 16:00:58 +0900

    feat: 헤더 컴포넌트 적용

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -2,3 +2,5 @@ import './main.scss';
 import './js/mainbanner.js';
 import './js/contentsPb.js';
 import './js/contentsSwiper.js';
+import './../../layouts/header/header.js';
+import './../../layouts/header/headerComponent.js';
