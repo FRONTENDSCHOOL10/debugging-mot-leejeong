976be304b53f365ebc7fc54fb96075ebb40db360
hyesu-jeong <hyeppy739@gmail.com>
2024-07-17 19:15:55 +0900

    feat: 컨텐츠 클릭 시 로그인 팝업 수정

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -2,6 +2,5 @@ import './main.scss';
 import './js/mainbanner.js';
 import './js/contentsPb.js';
 import './js/contentsSwiper.js';
-import './js/feature.js';
 import './../../layouts/header/header.js';
 import './../../layouts/footer/footer.js';
